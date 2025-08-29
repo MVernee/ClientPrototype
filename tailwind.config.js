22a38271304d8caf09677ca67653af166c1d4ce4
MVernee <mvernee@hso.com>
2025-08-29 14:25:35 +0200

    initial project setup with multi-app structure

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./applications/**/*.{js,jsx}",
+    "./shared/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
